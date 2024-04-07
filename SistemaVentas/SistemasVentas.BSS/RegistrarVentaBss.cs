ab3057533315bb8c575143c55582ad9b1d99c4ce
Nayeli <nayeliordonezchoque@gmail.com>
2024-04-06 22:47:56 -0400

    act

--- SistemaVentas/SistemasVentas.BSS/RegistrarVentaBss.cs
+++ SistemaVentas/SistemasVentas.BSS/RegistrarVentaBss.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemasVentas.DAL;
 
 namespace SistemasVentas.BSS
 {
     public class RegistrarVentaBss
     {
+       RegistrarVentaDal
     }
 }
